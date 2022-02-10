124569718b8c74784f59cbeb7494f96e6d459e8b
v1ztep <v1ztep@gmail.com>
2022-02-10 04:26:39 +0300

    Добавил модель DeliveryLocation

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -238,3 +238,18 @@ class OrderItem(models.Model):
                f'{self.order.firstname} ' \
                f'{self.order.lastname} ' \
                f'{self.order.address}'
+
+
+class DeliveryLocation(models.Model):
+    address = models.CharField(
+        verbose_name='адрес доставки',
+        max_length=200,
+        unique=True
+    )
+    lon = models.FloatField('долгота')
+    lat = models.FloatField('широта')
+    registered_at = models.DateTimeField(
+        default=timezone.now,
+        verbose_name='дата запроса к геокодеру',
+        db_index=True
+    )
