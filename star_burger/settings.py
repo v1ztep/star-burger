f753598da592f35edb484dc8443f9eb51a0ce25d
v1ztep <v1ztep@gmail.com>
2022-02-10 04:25:50 +0300

    Создал новый app location

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
     'restaurateur.apps.RestaurateurConfig',
+    'location.apps.LocationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
