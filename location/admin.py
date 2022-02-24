b1427798630ded0cb57b0f1378ab611ef3e9dc39
v1ztep <v1ztep@gmail.com>
2022-02-24 04:41:46 +0300

    Добавил модель RestaurantLocation

--- location/admin.py
+++ location/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 from location.models import DeliveryLocation
+from location.models import RestaurantLocation
 
 
 @admin.register(DeliveryLocation)
+class DeliveryLocation(admin.ModelAdmin):
+    list_display = ['address', 'lon', 'lat']
+
+
+@admin.register(RestaurantLocation)
 class DeliveryLocation(admin.ModelAdmin):
     list_display = ['address', 'lon', 'lat']
--- location/models.py
+++ location/models.py
@@ -15,3 +15,18 @@ class DeliveryLocation(models.Model):
         verbose_name='дата запроса к геокодеру',
         db_index=True
     )
+
+
+class RestaurantLocation(models.Model):
+    address = models.CharField(
+        verbose_name='адрес ресторана',
+        max_length=200,
+        unique=True
+    )
+    lon = models.FloatField('долгота', blank=True, null=True)
+    lat = models.FloatField('широта', blank=True, null=True)
+    registered_at = models.DateTimeField(
+        default=timezone.now,
+        verbose_name='дата запроса к геокодеру',
+        db_index=True
+    )
